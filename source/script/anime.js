422c6bb2f694f93222f923a16a2ab5ee29720b0e
PulseArc <61a61a71as@gmail.com>
2025-05-01 15:58:45 +0300

    Add files via upload

--- source/script/anime.js
+++ source/script/anime.js
@@ -605,7 +605,7 @@ function generateRandomCards() {
                         <div class="card-rating-trand" bis_skin_checked="1">
                             <span class="span-rating">${randomRating}</span>
                         </div>
-                        ${val.isTV ? '<div class="card-TV" bis_skin_checked="1">TV</div>' : ''}
+                        ${val.isTV ? '<div class="card-TV card-TV-trends" bis_skin_checked="1">TV</div>' : ''}
                         <div class="card-body">
                             <span class="card-tex">${val.name}<br><span class="year">${val.year}</span></span>
                         </div>
